d22ad6f78307f668b799e623100e8248de4faf6d
Nguyễn Thanh Hòa <83102917+xuanphao19@users.noreply.github.com>
2023-01-14 21:05:07 +0700

    15/90

--- assets/Js/fireworksMax.js
+++ assets/Js/fireworksMax.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
       window.setTimeout(callback, 1000 / 60);
     };
   let canvas, ctx, w, h;
-  let fireworksMax = 12;
-  let numberOfParticles = randomNumber(20, 80);
+  let fireworksMax = 5;
+  let numberOfParticles = randomNumber(15, 90);
   let fireworks = [],
     particles = [];
   let hue = 0;
